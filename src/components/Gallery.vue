fe1b38d435f6300c231e2ffad5215cd633aee7d9
brankaDakic10 <branka.dakic@yahoo.com>
2018-06-11 20:17:11 +0200

    Added Gallery in AllGalleries

---
+++ src/components/Gallery.vue
@@ -0,0 +1,33 @@
+   <template>
+     <div class="row">
+   
+    <div class="card" style="width: 40rem;">
+      
+      <div class="card-body">
+        
+        <h5 class="card-title"><i style="color: #086A87">Title:</i>{{ gallery.title}}</h5>
+        <h5 class="card-title"><i style="color: #086A87">Author: </i>{{gallery.firstName}} {{gallery.lastName}}</h5>
+        <h5 class="card-title"><i style="color: #086A87">Created_at: </i>{{ gallery.created_at}}</h5>
+
+      </div>
+    </div>
+ 
+  </div>
+</template>
+<script>
+
+export default {
+    name:"Gallery",
+    props:{
+        gallery:{
+      type: Object,
+      required:true
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/AllGalleries.vue
+++ src/pages/AllGalleries.vue
@@ -1,15 +1,27 @@
 <template>
     <div class="container">
         All Galeries
+        <div class="pt-4">
+            <gallery v-for="gallery in galleries" :key="gallery.id" :gallery="gallery" />
+            <div class="alert alert-warning" role="alert" v-if="!galleries.length">
+                <strong>Warning!</strong> There is no gallery created!
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-name:"AllGaleries"
-}
+    import Gallery from './../components/Gallery.vue'
+    export default {
+        name: "AllGaleries",
+        components: {
+            Gallery
+        },
+        data() {
+            galleries: []
+        }
+    }
 </script>
 
 <style>
-
 </style>
