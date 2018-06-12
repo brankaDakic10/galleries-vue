54c8d3d32fe19e763f3509c7b3b1d97695d85d7f
brankaDakic10 <branka.dakic@yahoo.com>
2018-06-12 21:23:03 +0200

    Added changes in CreateNewGallery.vue

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -2,12 +2,13 @@
      <div class="row">
    
     <div class="card" style="width: 40rem;">
-      
+       <img class="card-img-top" :src="gallery.imageUrl[0]" alt="Card image cap"
+      style="width: 5rem;">
       <div class="card-body">
         
         <h5 class="card-title"><i style="color: #086A87">Title:</i>{{ gallery.title}}</h5>
         <h5 class="card-title"><i style="color: #086A87">Author: </i>{{gallery.firstName}} {{gallery.lastName}}</h5>
-        <!-- <h5 class="card-title"><i style="color: #086A87">Created_at: </i>{{ gallery.created_at}}</h5> -->
+        <h5 class="card-title"><i style="color: #086A87">Created_at: </i>{{ gallery.created_at}}</h5>
 
       </div>
     </div>
--- src/pages/CreateNewGallery.vue
+++ src/pages/CreateNewGallery.vue
@@ -1,15 +1,76 @@
 <template>
     <div class="container">
-        CreateNewGallery
+        <h4 class="text-center">Create new gallery </h4>
+        <form @submit.prevent="storeGallery" @reset="onReset" class="jumbotron">
+            <div class="form-group row">
+                <label class="col-4 col-form-label" for="title">Title:</label>
+                <div class="col-8">
+                    <input id="title" name="title" type="text" class="form-control here" required="required" minlength="2" maxlength="255" v-model="newGallery.title">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-4 col-form-label" for="description">Description:</label>
+                <div class="col-8">
+                    <input id="description" name="description" type="text" class="form-control here" maxlength="1000" v-model="newGallery.description">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-4 col-form-label" for="imageUrl">Image url:</label>
+                <div class="col-8">
+                    <input id="imageUrl" name="imageUrl" type="text" class="form-control here" required="required" v-model="newGallery.imageUrl">
+                </div>
+            </div>
+             <div class="form-group row">
+                 <div class="col-4"></div>
+                <!-- <label class="col-4 col-form-label" for="imageUrl">Add new image url:</label> -->
+                <div class="col-8">
+              
+ <button  class="btn btn-default">Add another URL</button>
+
+                </div>
+            </div>
+            <div class="form-group row">
+
+                <div class="offset-4 col-8">
+
+                    <button name="reset" type="reset" class="btn btn-danger">Reset</button>
+                    <button class="btn btn-info" @click="cancel">Cancel</button>
+
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+
+        </form>
     </div>
 </template>
 
 <script>
-export default {
-name:"CreateNewGallery"
-}
+    export default {
+        name: "CreateNewGallery",
+        data() {
+            return {
+                newGallery: {
+                    title: "",
+                    description: "",
+                    imageUrl: ""
+                }
+            }
+        },
+        methods: {
+            storeGallery() {
+                //
+            },
+            onReset() {
+                this.newGallery = {}
+            },
+            cancel() {
+                this.$router.push({
+                    name: "my-galleries"
+                })
+            }
+        }
+    }
 </script>
 
 <style>
-
 </style>
